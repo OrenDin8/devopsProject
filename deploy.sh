007337fddde292e04c7cb77416da95cbf4b360ea
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-07 21:05:19 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ MACHINE=$1
 
 echo "Deploying to ${MACHINE} starting"
 
-echo "copying the project dir in $MACHINE machine"
+echo "Copying the docker-compose file in $MACHINE machine"
 scp -o StrictHostKeyChecking=no -r "$/var/lib/jenkins/workspace/Final_Project/docker-compose.yml" ec2-user@${MACHINE}:~
 
 ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} << 'EOF'
   cp .env Final_Project/
   cd /home/ec2-user/Final_Project/
   docker-compose up -d --no-build 
-  sleep 20
+  sleep 25
   if [ "$MACHINE" == "test" ];
   then
       HTTP=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:5000"`
