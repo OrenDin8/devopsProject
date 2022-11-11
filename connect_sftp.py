b6fea898cbc7491099236a6c2d77665510ad0824
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-11 05:25:03 +0200

    Update connect_sftp.py

--- connect_sftp.py
+++ connect_sftp.py
@@ -1,15 +1,22 @@
 import pysftp
 import warnings
+import os
+from dotenv import load_dotenv
+
+
 warnings.filterwarnings('ignore','.*Failed to load HostKeys.*')
 # create connection options object and ignore_known hosts check
 cnopts = pysftp.CnOpts()
 cnopts.hostkeys = None
-
-
+load_dotenv()
 # connection parameters
 def connect_to_sftp():
-    with pysftp.Connection(host='185.164.16.144', username='oreng', password='123456', cnopts=cnopts ) as sftp:
-        print("connected to sftp server!")
+   with pysftp.Connection(cnopts=cnopts, host=os.environ["host_machine_env"], username=os.environ["username_env"], password=os.environ["password_env"]) as sftp:
+	#print("connected to sftp server!")
         # dowmload remote folder files to local folder
         sftp.get_d("/var/tmp/csv_files/", localdir="./", preserve_mtime=True)
+
        
+       
+
+
