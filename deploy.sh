f2595a74f0ca51a97bbeb86c470cdd0c394047dc
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-12 22:43:39 +0200

    edit EOF

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,13 @@ echo "Deploying to ${MACHINE} start"
 echo "Copying the docker compose file in $MACHINE machine"
 scp -o StrictHostKeyChecking=no -r "$JENKINS_WORKSPACE"/docker-compose.yml ec2-user@${MACHINE}:~
 
-ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} << EOF
- cd /home/ec2-user/
- docker pull orendin8/devops_project:latest
- docker-compose up --build -d 
- sleep 25 
- if [ "$MACHINE" == "test" ];
-  then
-      if curl -I "http://127.0.0.1:5000" 2>&1 | grep -w "200\|301" ; then
-        	echo "page is working"
-    	else
-        	echo "page is down"
-    	fi
-  fi
-EOF
-
+ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cd /home/ec2-user/ && docker pull orendin8/devops_project:latest && docker-compose up --no-build -d && sleep 30"
+if [ "$MACHINE" == "test" ];
+ then
+     if curl -I "http://127.0.0.1:5000" 2>&1 | grep -w "200\|301" ; then
+       	echo "page is working"
+   	else
+       	echo "page is down"
+   	fi
+fi
 echo "Deploying to $MACHINE server succedded"
