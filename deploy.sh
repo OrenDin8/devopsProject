947448e480ec8675b9dbbd8286ed0039eb7c0643
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-12 23:12:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "Deploying to ${MACHINE} start"
 echo "Copying the docker compose file in $MACHINE machine"
 scp -o StrictHostKeyChecking=no -r "$JENKINS_WORKSPACE"/docker-compose.yml ec2-user@${MACHINE}:~
 
-ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cd /home/ec2-user/ && docker pull orendin8/devops_project:latest && docker-compose up --no-build -d && sleep 30"
+ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cd /home/ec2-user/ && docker pull orendin8/devops_project/docker/dockerfile1:latest && docker-compose up --no-build -d && sleep 30"
 if [ "$MACHINE" == "test" ];
  then
      if curl -I "http://127.0.0.1:5000" 2>&1 | grep -w "200\|301" ; then
