29e59c7c61c5f4bc610c3dfe0fddd2f7237f9fd4
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-13 05:55:05 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,11 @@ scp -o StrictHostKeyChecking=no -r "$JENKINS_WORKSPACE"/docker-compose.yml ec2-u
 ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cd /home/ec2-user/ && docker pull orendin8/devops_project:latest && docker-compose up --no-build -d && sleep 30"
 if [ "$MACHINE" == "test" ];
  then
-     if curl -I "http://127.0.0.1:5000" 2>&1 | grep -w "200\|301" ; then
-       	echo "page is working"
-   	else
-       	echo "page is down"
-   	fi
+     if curl -I "http://127.0.0.1:5000" 2>&1 | grep -w "200\|301" ;
+      then
+       	echo "The website is up "
+   	  else
+       	echo "The website is down"
+   	 fi
 fi
 echo "Deploying to $MACHINE server succedded"
