116234c87057f699dfdd6ce64a219f2337fec5e4
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-16 11:48:07 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,16 @@ echo "Copying the docker compose file in $MACHINE machine"
 scp -o StrictHostKeyChecking=no -r "$JENKINS_WORKSPACE"/docker-compose.yml ec2-user@${MACHINE}:~
 
 ssh -o StrictHostKeyChecking=no ec2-user@${MACHINE} "cd /home/ec2-user/ && docker pull orendin8/devops_project:latest && docker-compose up --no-build -d && sleep 30"
-if [ "$MACHINE" == "test" ];
- then
-     if curl -I "http://${TEST}:5000" 2>&1 | grep -w "200\|301" ;
+
+  if [ "$MACHINE" == "test" ];
+  then
+      HTTP=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:5000"`
+      echo $HTTP
+      if [ "$HTTP" == "200" ];
       then
-       	echo "The website is up "
-   	  else
-       	echo "The website is down"
-   	 fi
-fi
+  echo "Test succedded"
+      else
+  echo "Test failed"
+      fi
+  fi
 echo "Deploying to $MACHINE server succedded"
