0c7b34d4a3e82ee59bdee424af8c17714fad956f
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-20 00:12:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,20 @@
 HOME_DIR="/home/ec2-user"
 JENKINS_WORKSPACE="/var/lib/jenkins/workspace/Final_Project"
 MACHINE=$1
+IP_PUBLIC='ec2-44-201-110-248.compute-1.amazonaws.com'
 if [ "$MACHINE" == 'test' ];
  then 
-     MACHINE='ec2-44-201-110-248.compute-1.amazonaws.com'
+     IP_PUBLIC='ec2-44-201-110-248.compute-1.amazonaws.com'
 	 echo "Deploying to test start"
 else 
-     MACHINE='ec2-54-163-62-126.compute-1.amazonaws.com'
+     IP_PUBLIC='ec2-54-163-62-126.compute-1.amazonaws.com'
 	 echo "Deploying to production start"
 
 fi
-echo "Copying the docker compose file in $MACHINE machine"
-scp -o StrictHostKeyChecking=no -r "$JENKINS_WORKSPACE"/docker-compose.yml ec2-user@${MACHINE}:~
+echo "Copying the docker compose file to the machine"
+scp -i Devops-course.pem "$JENKINS_WORKSPACE"/docker-compose.yml ec2-user@${IP_PUBLIC}:~
 
-ssh -i Devops-course.pem ec2-user@{MACHINE} "cd /home/ec2-user/ && docker pull orendin8/devops_project:latest && docker-compose up --no-build -d && sleep 30"
+ssh -i Devops-course.pem ec2-user@{IP_PUBLIC} "cd /home/ec2-user/ && docker pull orendin8/devops_project:latest && docker-compose up --no-build -d && sleep 30"
 
 if [ "$MACHINE" == "test" ];
  then
