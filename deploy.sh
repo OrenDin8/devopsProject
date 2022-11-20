4ea604488873a0adf7908b5def2037bdf7043ccc
OrenDin8 <39104811+OrenDin8@users.noreply.github.com>
2022-11-20 18:31:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,21 +15,16 @@ else
 	 echo "Deploying to production start"
 fi
 echo "Copying the docker compose file to the machine"
+
 scp -i "Devops-course.pem"  -o StrictHostKeyChecking=no "$JENKINS_WORKSPACE"/docker-compose.yml ec2-user@${IP_PUBLIC}:~
 scp -i "Devops-course.pem" -o StrictHostKeyChecking=no "$JENKINS_WORKSPACE"/.env ec2-user@${IP_PUBLIC}:~
 
 ssh -i "Devops-course.pem" -o StrictHostKeyChecking=no ec2-user@${IP_PUBLIC} "cd /home/ec2-user/ && docker pull orendin8/devops_project:latest && docker-compose up --no-build -d && sleep 30"
 
-HTTP_CODE_STATUS=`curl -o /dev/null -s -w "%{http_code}\n" http://localhost:5000/`	
+
 if [ "$MACHINE" == "test" ];	     
-then
-	if [ $HTTP_CODE_STATUS -eq 200 ];
-	then
-		echo "The web is up"
-	else
-		echo "The web is down"
-		exit 1
-	fi
+ then
+     ssh -i "Devops-course.pem" -o StrictHostKeyChecking=no ec2-user@${IP_PUBLIC} "curl -I "http://127.0.0.1:5000""
 fi
 echo "Deploying to $MACHINE server succedded"
 
